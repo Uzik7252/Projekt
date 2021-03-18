adc682cde852feeb832fae6dfeaabc311a1471b2
GoldenSlav <79305258+GoldenSlav@users.noreply.github.com>
2021-03-18 17:57:42 +0100

    Dodaje Historię

--- test/src/main.java
+++ test/src/main.java
@@ -1,7 +1,42 @@
-public class main {
+import java.util.Random;
+
+class RandomNumberGenerator {
+    public static String usage = "Usage: java RandomNumberGenerator <lines> [seed]";
+
     public static void main(String[] args) {
-        System.out.println("siemka");
-        System.out.println("loda niemcu");
+        if (args.length > 2) {
+            System.err.println("Too many arguments!");
+            System.err.println(usage);
+            System.exit(1);
+        } else if (args.length == 0) {
+            System.err.println("Requires at least one argument");
+            System.err.println(usage);
+            System.exit(1);
+        }
+
+        int count = parseCriticalInt(args[0]);
+
+        Random rng;
+        if (args.length == 2) {
+            int seed = parseCriticalInt(args[1]);
+            rng = new Random(seed);
+        } else {
+            rng = new Random();
+        }
+
+        for (int i = 0; i < count; ++i) {
+            System.out.println(rng.nextInt(9));
+        }
     }
 
+    public static int parseCriticalInt(String stringValue) {
+        int intValue = 0;
+        try {
+            intValue = Integer.parseInt(stringValue);
+        } catch (NumberFormatException e) {
+            System.err.println(stringValue + " must be an integer");
+            System.exit(2);
+        }
+        return intValue;
+    }
 }
