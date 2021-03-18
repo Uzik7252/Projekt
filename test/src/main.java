29d89a84f30a73ffa2e26329605bf9fe265dc873
Uzik7252 <79507509+Uzik7252@users.noreply.github.com>
2021-03-18 17:45:10 +0100

    dodaję gracza

--- test/src/main.java
+++ test/src/main.java
@@ -1,16 +1,13 @@
 public class main {
-<<<<<<< HEAD
+
     public static void main(String[] args) throws InterruptedException {
 
 
         kostka kostka = new kostka();
 
         kostka.sześć_ścian();
-=======
-    public static void main(String[] args) {
-        System.out.println("siemka");
-        System.out.println("loda niemcu");
->>>>>>> f460662a1fc25d9e63cdcb4319fb847bbb8a5d33
+
+
     }
 
 }
